5c99075d35d6bf0ef1e327d07eef25424e387ee2
Pramod <82228576+pramod-k-codes@users.noreply.github.com>
2023-12-18 16:50:42 +0530

    date time "

--- mongodb.sh
+++ mongodb.sh
@@ -1,56 +1,8 @@
 #!/bin/bash
 
-ID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
+CURRENT_FILE_NAME = $0
+CURRENT_DATE = date +"%Y-%m-%d_%H:%M:%S"
+LOGFILE = /tmp/$CURRENT_FILE_NAME-$CURRENT_DATE #filename timestamp logfile
 
-TIMESTAMP=$(date +%F-%H-%M-%S)
-LOGFILE="/tmp/$0-$TIMESTAMP.log"
-
-echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
-
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-        echo -e "$2 ... $R FAILED $N"
-        exit 1
-    else
-        echo -e "$2 ... $G SUCCESS $N"
-    fi
-}
-
-if [ $ID -ne 0 ]
-then
-    echo -e "$R ERROR:: Please run this script with root access $N"
-    exit 1 # you can give other than 0
-else
-    echo "You are root user"
-fi # fi means reverse of if, indicating condition end
-
-cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
-
-VALIDATE $? "Copied MongoDB Repo"
-
-dnf install mongodb-org -y &>> $LOGFILE
-
-VALIDATE $? "Installing MongoDB"
-
-systemctl enable mongod &>> $LOGFILE
-
-VALIDATE $? "Enabling MongoDB"
-
-systemctl start mongod &>> $LOGFILE
-
-VALIDATE $? "Starting MongoDB"
-
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>> $LOGFILE
-
-VALIDATE $? "Remote access to MongoDB"
-
-systemctl restart mongod &>> $LOGFILE
-
-VALIDATE $? "Restarting MongoDB"
-
-
+# vim /etc/yum.repos.d/mongo.repo
+cp monorepo /etc/yum.repos.d/mongo.repo >>
